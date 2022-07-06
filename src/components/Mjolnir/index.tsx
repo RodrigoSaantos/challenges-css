9dc9a202e779dca1b30d8cbe0b51ca9cef044be0
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-07-06 00:47:22 -0300

    feat: initial mjolnir

---
+++ src/components/Mjolnir/index.tsx
@@ -0,0 +1,40 @@
+import './styles.css';
+
+export function Mjolnir() {
+  return (
+    <div className="container">
+      <div className="mjolnir">
+        <div className="hammer-container">
+          <div className="hammer-left">
+            <div className="hammer-left-inside"></div>
+          </div>
+          <div className="hammer-center">
+            <div className="hammer-top"></div>
+            <div className="hammer">
+              <div className="header">
+                <div className="quadrado"></div>
+              </div>
+            </div>
+            <div className="hammer-bottom"></div>
+          </div>
+          <div className="hammer-right">
+            <div className="hammer-right-inside"></div>
+          </div>
+        </div>
+        <div className="cano">
+          <div className="cano-header">
+
+          </div>
+          <div className="cano-header-bottom"></div>
+          <div className="wood">
+            <div className="cano-silver"></div>
+
+          </div>
+          {/* <div className="cano-top"></div>
+          <div className="cano-center"></div>
+          <div className="cano-silver"></div> */}
+        </div>
+      </div>
+    </div>
+  )
+}
