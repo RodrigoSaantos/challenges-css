0c03a85791ee150d2aa17241f329ff56a883d45f
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-07-06 00:46:41 -0300

    feat: sharingan

---
+++ src/components/Sharingan/index.tsx
@@ -0,0 +1,22 @@
+import './styles.css'
+export function Sharingan() {
+  return (
+    <div className="container">
+      <div className="sharingan">
+        <div className="tomoe-center"></div>
+        <div className="circle-center">
+          <div className="position"></div>
+          <div className="tomoe-container">
+            <div className="tomoe-content">
+              <div className="tomoe tomoe-one">
+                <div className="tomoe-curve"></div>
+                <div className="tomoe-background"></div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
